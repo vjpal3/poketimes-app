b3ad5f84c82b0109ef3b9b21a74205e33a7c131f
Vrishali <vrishalipal@yahoo.com>
2019-05-11 18:11:23 -0400

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">This is the About Page</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit sed minima deleniti a accusamus quod delectus dolore, tempora quaerat magnam quasi quibusdam in sint, culpa deserunt ex ipsam. Neque, soluta?</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000);
+    return (
+        <div className="container">
+            <h4 className="center">This is the Contact Page</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit sed minima deleniti a accusamus quod delectus dolore, tempora quaerat magnam quasi quibusdam in sint, culpa deserunt ex ipsam. Neque, soluta?</p>
+        </div>
+    )
+}
+
+export default Contact;
